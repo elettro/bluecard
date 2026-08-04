e41aa40142029faaecd2f110409afcca7a539cd9
elettro <dpalermo@elettro.com>
2026-08-04 13:36:34 -0400

    Remove homepage supporters section

--- global-layout.js
+++ global-layout.js
@@ -56,13 +56,16 @@
 </div>`;
       }
     }
+
+    var supportersSection = document.querySelector('main .homepage-supporters');
+    if (supportersSection) supportersSection.remove();
   }
 
   var baseScript = document.createElement('script');
-  baseScript.src = '/bluecard/global-layout-base.js?v=20260804-4';
+  baseScript.src = '/bluecard/global-layout-base.js?v=20260804-5';
   baseScript.onload = function () {
     var homepageScript = document.createElement('script');
-    homepageScript.src = '/bluecard/homepage-how-we-help.js?v=20260804-4';
+    homepageScript.src = '/bluecard/homepage-how-we-help.js?v=20260804-5';
     document.body.appendChild(homepageScript);
   };
   document.head.appendChild(baseScript);
