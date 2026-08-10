6bd1e926599c32c42ec7b42e90d641e5474e296f
elettro <dpalermo@elettro.com>
2026-08-10 12:02:59 -0400

    Make global layout child scripts cache-safe

--- global-layout.js
+++ global-layout.js
@@ -1,9 +1,10 @@
 (function () {
+  var cacheBust = Date.now();
   var legacyScript = document.createElement('script');
-  legacyScript.src = '/bluecard/global-layout-legacy.js?v=20260810-bbb1';
+  legacyScript.src = '/bluecard/global-layout-legacy.js?v=' + cacheBust;
   legacyScript.onload = function () {
     var bbbPatch = document.createElement('script');
-    bbbPatch.src = '/bluecard/bbb-footer.js?v=20260810-bbb1';
+    bbbPatch.src = '/bluecard/bbb-footer.js?v=' + cacheBust;
     document.body.appendChild(bbbPatch);
   };
   document.head.appendChild(legacyScript);
